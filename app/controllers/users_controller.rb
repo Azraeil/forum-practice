7c6ad1151daeca51928b6f27bf2b8ffb4408d42d
Shih_Kuan <cliff1304@gmail.com>
2018-04-21 22:47:35 +0800

    Create users#edit and #show view.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,12 @@
 class UsersController < ApplicationController
+  before_action :set_user
+  def show
+    # before action set_user
+  end
+
+  private
+  def set_user
+    @user = User.find(params[:id])
+    return @user
+  end
 end
