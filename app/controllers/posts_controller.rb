6ee1472bf0b01ac4b34e0d048243fa055c2ce256
Shih_Kuan <cliff1304@gmail.com>
2018-04-23 16:24:43 +0800

    Modify controller and view of posts#new page.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,18 @@ class PostsController < ApplicationController
   # 依需求，瀏覽論壇首頁時，不用登入
   skip_before_action :authenticate_user!, only: [:index]
 
+  before_action :set_post, only: [:create]
+
   def index
     @posts = Post.last(10)
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  private
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
