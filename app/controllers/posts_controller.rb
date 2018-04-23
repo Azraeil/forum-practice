405cf3a6f316e13788e153e6c3f0cb26d81f8929
Shih_Kuan <cliff1304@gmail.com>
2018-04-23 15:15:07 +0800

    Modify posts#index to show posts list.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,8 @@
 class PostsController < ApplicationController
   # 依需求，瀏覽論壇首頁時，不用登入
   skip_before_action :authenticate_user!, only: [:index]
-  def index
 
+  def index
+    @posts = Post.last(10)
   end
 end
