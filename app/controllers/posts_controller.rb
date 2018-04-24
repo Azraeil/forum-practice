273111e2d680b7ac86c3d4bd90e2f0c1dc09f21f
Shih_Kuan <cliff1304@gmail.com>
2018-04-24 12:32:56 +0800

    Modify posts#edit page and posts#update method.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   # 依需求，瀏覽論壇首頁時，不用登入
   skip_before_action :authenticate_user!, only: [:index]
 
-  before_action :set_post, only: [:show]
+  before_action :set_post, only: [:show, :edit, :update]
 
   def index
     @posts = Post.last(10)
@@ -17,7 +17,7 @@ class PostsController < ApplicationController
 
     if @post.save
       flash[:notice] = "Post was created successfully."
-      redirect_to post_url(@post.id)
+      redirect_to post_path(@post.id)
     else
       flash[:alert] = "Post was failed to create."
       render :new
@@ -26,6 +26,25 @@ class PostsController < ApplicationController
 
   def show
 
+  end
+
+  def edit
+    # set_post
+
+    #dev 只有作者才能編輯文章
+
+  end
+
+  def update
+    # set_post
+
+    if @post.update(post_params)
+      flash[:notice] = "Post data was successfully updated."
+      redirect_to post_path(@post.id)
+    else
+      flash.now[:alert] = "Post was failed to update!"
+      render :edit
+    end
   end
 
   private
