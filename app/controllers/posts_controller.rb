344b7582917aa402442dd7009eee852e16814c0f
Shih_Kuan <cliff1304@gmail.com>
2018-04-24 10:59:51 +0800

    Fix posts#new cannot save post.who_can_see issue. Finish posts#create.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   # 依需求，瀏覽論壇首頁時，不用登入
   skip_before_action :authenticate_user!, only: [:index]
 
-  before_action :set_post, only: [:create]
+  before_action :set_post, only: [:show]
 
   def index
     @posts = Post.last(10)
@@ -12,8 +12,28 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      flash[:notice] = "Post was created successfully."
+      redirect_to post_url(@post.id)
+    else
+      flash[:alert] = "Post was failed to create."
+      render :new
+    end
+  end
+
+  def show
+
+  end
+
   private
   def set_post
     @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :content, :file, :who_can_see)
   end
 end
