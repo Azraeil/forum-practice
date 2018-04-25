7148dd5f44d3df40b92530f432e002a254b5aa22
Shih_Kuan <cliff1304@gmail.com>
2018-04-25 11:10:17 +0800

    Modify admin::categories#create.

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,5 +1,32 @@
 class Admin::CategoriesController < Admin::BaseController
   def index
     @categories = Category.all
+
+    # 根據有無傳入 params[:id] 來判斷實例變數的形態
+    if params[:id]
+      # edit category
+      @category = Category.find(params[:id])
+    else
+      # new category
+      @category = Category.new
+    end
+  end
+
+  def create
+    @category = Category.new(category_params)
+    puts category_params
+    if @category.save
+      flash[:notice] = "Category was successfully created!"
+      redirect_to admin_categories_path
+    else
+      flash[:alert] = "Category was failed to create."
+      @categories = Category.all
+      render :index
+    end
+  end
+
+  private
+  def category_params
+    params.require(:category).permit(:name)
   end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,6 @@
 class Category < ApplicationRecord
+  # name 欄位必須要填寫
+  validates_presence_of :name
+
   has_many :posts
 end
