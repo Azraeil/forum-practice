547f48c4b3607af5549dfd8f75761d0fa07c129e
Shih_Kuan <cliff1304@gmail.com>
2018-04-25 11:35:36 +0800

    Modify admin::categories#edit, #update, #destroy and set dependency of category and posts.

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,4 +1,6 @@
 class Admin::CategoriesController < Admin::BaseController
+  before_action :set_category, only: [:update, :destroy]
+
   def index
     @categories = Category.all
 
@@ -25,8 +27,34 @@ class Admin::CategoriesController < Admin::BaseController
     end
   end
 
+  def update
+    # set_category
+
+    if @category.update(category_params)
+      redirect_to admin_categories_path, notice: "Category data was successfully updated."
+    else
+      flash.now[:alert] = "Category was failed to update!"
+      @categories = Category.all
+      render :index
+    end
+  end
+
+  def destroy
+    if @category.destroy
+      flash[:notice] = "Category was successfully deleted."
+    else
+      flash[:alert] = @category.errors.full_messages.to_sentence
+    end
+
+    redirect_to admin_categories_path
+  end
+
   private
   def category_params
     params.require(:category).permit(:name)
+  end
+
+  def set_category
+    @category = Category.find(params[:id])
   end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -2,5 +2,5 @@ class Category < ApplicationRecord
   # name 欄位必須要填寫
   validates_presence_of :name
 
-  has_many :posts
+  has_many :posts, dependent: :restrict_with_error
 end
