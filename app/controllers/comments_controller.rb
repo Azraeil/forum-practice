6d4609c84e4b1d0e12242a40c8e8a32260ac0b20
Shih_Kuan <cliff1304@gmail.com>
2018-04-26 13:57:12 +0800

    User can create comment in posts#show page.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,30 @@
 class CommentsController < ApplicationController
+  def create
+    @post = Post.find(params[:post_id])
+
+    if @comment = Comment.create!(user_id: current_user.id, post_id: params[:post_id], content: comment_params[:content])
+      flash[:notice] = "Comment was created successfully."
+    else
+      flash[:alert] = "Comment was failed to create."
+    end
+
+    redirect_to post_path(@post.id)
+  end
+
+  def destroy
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
 end
