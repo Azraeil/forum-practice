da2ab2e2873e2aa9df270fbe95bb7dffa5b51fb3
Shih_Kuan <cliff1304@gmail.com>
2018-04-28 19:50:58 +0800

    Add generate_authentication_token method to user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  :recoverable, :rememberable, :trackable, :validatable
 
-  # 檢查是否爲網站管理員
-  def is_admin?
-   if self.role == "Admin"
-      return true
-   end
-  end
+  before_create :generate_authentication_token
 
   has_many :posts
 
@@ -21,7 +16,18 @@ class User < ApplicationRecord
   # 使用者有很多在收藏記錄裡的的文章
   has_many :collected_posts, through: :collects, source: :post
 
+  # 檢查是否爲網站管理員
+  def is_admin?
+   if self.role == "Admin"
+      return true
+   end
+  end
+
   def is_collected?(post)
     self.collected_posts.include?(post)
+  end
+
+  def generate_authentication_token
+    self.authentication_token = Devise.friendly_token
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180427055902) do
+ActiveRecord::Schema.define(version: 20180428114820) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name", null: false
@@ -64,6 +64,8 @@ ActiveRecord::Schema.define(version: 20180427055902) do
     t.datetime "updated_at", null: false
     t.string "introduction"
     t.integer "comments_count", default: 0
+    t.string "authentication_token"
+    t.index ["authentication_token"], name: "index_users_on_authentication_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
