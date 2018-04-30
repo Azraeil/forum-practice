4fe4686d75ef9af035cd1c056d2313c48ef62fe6
Shih_Kuan <cliff1304@gmail.com>
2018-04-30 14:05:32 +0800

    Set relationship between post, category and categories_post.

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,5 +2,5 @@ class Category < ApplicationRecord
   # name 欄位必須要填寫
   validates_presence_of :name
 
-  has_many :posts, dependent: :restrict_with_error
+  has_many :categories_posts, :dependent => :destroy
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ApplicationRecord
-  belongs_to :category
+  has_many :categories_posts, :dependent => :destroy
+
+  has_many :categories, through: :categories_posts, source: :category
 
   belongs_to :user
   has_many :comments, :dependent => :destroy
