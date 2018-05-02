67a06faa6b25cef3391d728404ef82d75ebbb5a2
Shih_Kuan <cliff1304@gmail.com>
2018-05-02 16:20:06 +0800

    rails g model Friend.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180430141330) do
+ActiveRecord::Schema.define(version: 20180502081722) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name", null: false
@@ -38,6 +38,14 @@ ActiveRecord::Schema.define(version: 20180430141330) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "post_id"
+  end
+
+  create_table "friends", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "friend_id"
+    t.string "status", default: "wait"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "posts", force: :cascade do |t|
