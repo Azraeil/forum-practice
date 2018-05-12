bfbf545e8c4ae145943924617c782cf5feb3656e
Shih_Kuan <cliff1304@gmail.com>
2018-05-12 14:58:19 +0800

    若該 category 底下還有 post 則不能刪除。

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,7 +2,8 @@ class Category < ApplicationRecord
   # name 欄位必須要填寫
   validates_presence_of :name
 
-  has_many :categories_posts, :dependent => :destroy
+  has_many :categories_posts
 
-  has_many :posts, through: :categories_posts
+  # 若發現該 category 底下還有 post 則不能刪除
+  has_many :posts, through: :categories_posts, :dependent => :restrict_with_error
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
+  # 刪除文章時一併刪除 文章的分類記錄
   has_many :categories_posts, :dependent => :destroy
 
   has_many :categories, through: :categories_posts
